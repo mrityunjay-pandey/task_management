af37f732a2b1b42309b0ab869fafc3c97a562749
Dev <dev@example.com>
2026-08-10 06:51:55 +0000

    chore: wire TasksModule and ProjectsModule into AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,8 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
   imports: [
@@ -12,6 +14,8 @@ import { UsersModule } from './users/users.module';
     PrismaModule,
     AuthModule,
     UsersModule,
+    TasksModule,
+    ProjectsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
